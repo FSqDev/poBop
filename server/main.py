f84a5fbdfc0e054afce07c74a17685021cd7aae0
Poplar Wang <wang.poplar@gmail.com>
2021-01-09 18:14:41 -0500

    MongoDB for server

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response, jsonify
+import db
 
 app = Flask("poBop")
 
@@ -6,6 +7,18 @@ app = Flask("poBop")
 def home():
     """ Basically just here to check if server is running """
     return 'poBop server is running'
+
+
+@app.route('/users/register', methods=['POST'])
+def register():
+    """ Securely registers a user """
+
+
+@app.route("/testdb")
+def test():
+    db.db["collectionname"].insert_one({"param": "value"})
+    return "Connected to the data base!"
+
 
 if __name__ == "__main__":
     app.run(port=33507)
