25deb1d8340ec035f3638c952008607a10a70e4b
Richard Qin <rqin1@ualberta.ca>
2021-01-10 02:19:09 -0700

    we are homeless now

--- android/app/src/main/java/com/fsq/pobop/MainActivity.java
+++ android/app/src/main/java/com/fsq/pobop/MainActivity.java
@@ -6,12 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -23,11 +18,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ConstraintLayout mBottomSheet;
     private BottomSheetBehavior mBottomSheetBehavior;
-    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +42,15 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_pantry, R.id.nav_recipe)
+                R.id.nav_pantry, R.id.nav_recipe, R.id.nav_qr)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
         // Some stuff for the filter popup
-        mBottomSheet = findViewById(R.id.bottom_sheet);
-        mButton = findViewById(R.id.extended_fab);
+        ConstraintLayout mBottomSheet = findViewById(R.id.bottom_sheet);
+        Button mButton = findViewById(R.id.extended_fab);
 
         mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
 
