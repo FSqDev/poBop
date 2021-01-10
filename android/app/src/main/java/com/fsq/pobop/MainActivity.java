79966669203381a669466edc7393986a1df54f9b
Richard Qin <rqin1@ualberta.ca>
2021-01-10 02:02:48 -0700

    log in 3 times and it will work dont worry

--- android/app/src/main/java/com/fsq/pobop/MainActivity.java
+++ android/app/src/main/java/com/fsq/pobop/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -71,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         SharedPreferences sharedPreferences = getSharedPreferences("auth", Context.MODE_PRIVATE);
+        Log.d("DOG",sharedPreferences.getString("id", "DOG"));
         if (sharedPreferences.getString("id", null) == null) {
             Intent newIntent = new Intent(MainActivity.this, AuthenticationActivity.class);
             startActivity(newIntent);
