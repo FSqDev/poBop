bae8d11e3f0f2e874b498e5f485fa1f2e5829654
Poplar Wang <wang.poplar@gmail.com>
2021-01-09 19:16:12 -0500

    Registration and auth

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response, jsonify
 import db
+import bcrypt
 
 app = Flask("poBop")
 
@@ -11,13 +12,32 @@ def home():
 
 @app.route('/users/register', methods=['POST'])
 def register():
-    """ Securely registers a user """
+    """ Registers new user account
+    Expecting body { email: str, password: str } """
+    if db.db["users"].find({"email": request.json["email"]}).count() != 0:
+        return Response("Email already registered", status=400)
+
+    salt = bcrypt.gensalt()
+    password_hashed = bcrypt.hashpw(request.json["password"].encode('utf8'), salt)
+    ret = db.db["users"].insert_one({"email": request.json["email"], "password": password_hashed})
+    return Response("Registration success", status=200)
 
 
-@app.route("/testdb")
-def test():
-    db.db["collectionname"].insert_one({"param": "value"})
-    return "Connected to the data base!"
+@app.route('/users/login', methods=['POST'])
+def login():
+    """ Attempts to login
+    Expecting body { email: str, password: str } """
+    accounts = db.db["users"].find({"email": request.json["email"]})
+
+    if accounts.count() == 0:
+        return Response("Email is not registered", status=400)
+    
+    for account in accounts: # Should always just be one
+        print(account)
+        if bcrypt.checkpw(request.json["password"].encode('utf8'), account["password"]):
+            return Response(str(account["_id"]), status=200)
+        else:
+            return Response("Wrong password", status=400)
 
 
 if __name__ == "__main__":
