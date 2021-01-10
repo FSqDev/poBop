c94a0a743376d9e8c794d125861c2dfc558ed200
Richard Qin <rqin1@ualberta.ca>
2021-01-09 23:16:17 -0700

    Update RecipeFragment.java

--- android/app/src/main/java/com/fsq/pobop/ui/recipe/RecipeFragment.java
+++ android/app/src/main/java/com/fsq/pobop/ui/recipe/RecipeFragment.java
@@ -7,16 +7,22 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.fsq.pobop.R;
+import com.fsq.pobop.ui.pantry.PantryViewModel;
 
 public class RecipeFragment extends Fragment implements RecipeRecViewAdapter.OnItemClickListener {
+
+    private RecipeViewModel viewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.recipe_fragment, container, false);
+
+        viewModel = new ViewModelProvider(this).get(RecipeViewModel.class);
 
         RecyclerView recyclerView = root.findViewById(R.id.recyclerViewRecipes);
         final RecipeRecViewAdapter adapter = new RecipeRecViewAdapter(this);
