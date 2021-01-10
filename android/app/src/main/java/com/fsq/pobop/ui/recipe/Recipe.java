1d8c03b4840b30f533507a4760f0e5eb28031382
Anmol Dhaliwal <AnmolDhaliwal00@users.noreply.github.com>
2021-01-09 17:50:01 -0700

    Added Java classes for recipes

---
+++ android/app/src/main/java/com/fsq/pobop/ui/recipe/Recipe.java
@@ -0,0 +1,36 @@
+package com.fsq.pobop.ui.recipe;
+
+public class Recipe {
+
+    private String name;
+    private String imageUrl;
+
+    public Recipe(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "name='" + name + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
