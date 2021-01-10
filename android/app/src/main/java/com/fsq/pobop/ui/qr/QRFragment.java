d77c7442049339a2669bc04134f08c4c94017ceb
Richard Qin <rqin1@ualberta.ca>
2021-01-10 03:27:02 -0700

    nav to pantry after scan

--- android/app/src/main/java/com/fsq/pobop/ui/qr/QRFragment.java
+++ android/app/src/main/java/com/fsq/pobop/ui/qr/QRFragment.java
@@ -9,12 +9,14 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.fsq.pobop.R;
 import com.fsq.pobop.data.DateConverter;
 import com.fsq.pobop.entity.ingredient.Ingredient;
+import com.fsq.pobop.ui.pantry.PantryFragmentDirections;
 
 public class QRFragment extends Fragment {
     private CodeScanner mCodeScanner;
@@ -38,6 +40,7 @@ public class QRFragment extends Fragment {
             ingredient.setExpiryDate(dateConverter.stringToDate(results[1]));
             ingredient.setDirty(1);
             viewModel.add(ingredient);
+            Navigation.findNavController(root).navigate(QRFragmentDirections.actionNavQrToNavPantry());
         }));
         scannerView.setOnClickListener(view -> mCodeScanner.startPreview());
         return root;
