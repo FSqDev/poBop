7275762c30276f71e98feb0c2438108ebe4b7764
Poplar Wang <wang.poplar@gmail.com>
2021-01-09 20:04:08 -0500

    dotenv

--- server/db.py
+++ server/db.py
@@ -2,5 +2,5 @@ import os
 from flask import Flask
 from flask_pymongo import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://Admin:lZ3wBWPxo0tKiikW@cluster0.ssnyz.mongodb.net/appdb?retryWrites=true&w=majority")
+client = pymongo.MongoClient(os.environ["MONGO_CLIENT"])
 db = client.get_database('flask_mongodb_atlas')
